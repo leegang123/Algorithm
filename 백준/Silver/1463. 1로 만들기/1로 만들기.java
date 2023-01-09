cf943bfe568915bb649afb344ebe187a698d6e5e
Gang-Lee <97741391+leegang123@users.noreply.github.com>
2023-01-09 13:42:28 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18048 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,40 +2,47 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        final int size = 1000000;
-        int[] dp = new int[size+1];
+        /*
+         * 문제 : 정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 할 때, 연산을 사용하는 횟수의 최솟값을 출력.
+         * 
+         * 접근 : 일단 큰 문제는 정수 N을 1로 만드는 연산의 최솟값을 구하는 문제이므로, 1부터 시작하는 바텀업 방식으로 각 숫자를 1로 만드는
+         * 횟수의 최솟값을 memo배열에 저장해 놓으면 각각의 경우에 1번 계산, 2번 계산, 3번 계산의 최솟값으로 쉽게 구할 수 있다.
+         * 
+         */
 
-        dp[1] = 0;
-        dp[2] = 1;
-        dp[3] = 1;
+        int N = stoi(br.readLine());
+        int[] memo = new int[N + 1];
 
-        for (int i = 4; i <= size; i++) {
-            dp[i] = Integer.MAX_VALUE;
-
+        memo[1] = 0;
+        for (int i = 2; i <= N; i++) {
+            int curValue = Integer.MAX_VALUE;
             if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i/3], dp[i]);
+                curValue = Math.min(curValue, memo[i / 3]);
             }
 
             if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i/2], dp[i]);
+                curValue = Math.min(curValue, memo[i / 2]);
             }
 
-            dp[i] = Math.min(dp[i-1], dp[i]);
-            dp[i]++;
+            curValue = Math.min(curValue, memo[i - 1]);
+            memo[i] = ++curValue;
         }
 
-        int N = stoi(br.readLine());
-
-        sb.append(dp[N] + "\n");
+        sb.append(memo[N]);
 
         br.close();
         System.out.println(sb);
     }
 
-    public static int stoi(String str) {return Integer.valueOf(str);}
-    public static long stol(String str) {return Long.valueOf(str);}
+    public static int stoi(String str) {
+        return Integer.valueOf(str);
+    }
+
+    public static long stol(String str) {
+        return Long.valueOf(str);
+    }
 }
